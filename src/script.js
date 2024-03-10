73d4538b51b05e189e644f2532c96647334db24c
Sagar <sagar.ahirrao@ltts.com>
2024-03-10 20:16:41 +0530

    code cleaning

--- src/script.js
+++ src/script.js
@@ -14,9 +14,9 @@ const gui = new GUI()
 //// DRACO LOADER TO LOAD DRACO COMPRESSED MODELS FROM BLENDER
 const dracoLoader = new DRACOLoader()
 const loader = new GLTFLoader()
-dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
-dracoLoader.setDecoderConfig({ type: 'js' })
-loader.setDRACOLoader(dracoLoader)
+// dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+// dracoLoader.setDecoderConfig({ type: 'js' })
+// loader.setDRACOLoader(dracoLoader)
 
 /////////////////////////////////////////////////////////////////////////
 ///// DIV CONTAINER CREATION TO HOLD THREEJS EXPERIENCE
@@ -26,7 +26,7 @@ document.body.appendChild(container)
 /////////////////////////////////////////////////////////////////////////
 ///// SCENE CREATION
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('#c8f0f9')
+scene.background = new THREE.Color('#f2fdff')
 
 /////////////////////////////////////////////////////////////////////////
 ///// RENDERER CONFIG
@@ -46,7 +46,7 @@ document.body.appendChild(labelRenderer.domElement)
 /////////////////////////////////////////////////////////////////////////
 ///// CAMERAS CONFIG
 const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100)
-camera.position.set(3, 0, -5)
+camera.position.set(0, 0, -5)
 scene.add(camera)
 
 /////////////////////////////////////////////////////////////////////////
@@ -66,7 +66,7 @@ window.addEventListener('resize', () => {
 /////////////////////////////////////////////////////////////////////////
 ///// CREATE ORBIT CONTROLS
 const controls = new OrbitControls(camera, renderer.domElement)
-
+setOrbitControlsLimits();
 
 /////////////////////////////////////////////////////////////////////////
 ///// RAYCASTER 
@@ -74,20 +74,16 @@ const raycaster = new THREE.Raycaster()
 
 /////////////////////////////////////////////////////////////////////////
 ///// SCENE LIGHTS
-const ambient = new THREE.AmbientLight(0xa0a0fc, 0.82)
+const ambient = new THREE.AmbientLight(0xa0a0fc, 1)
 scene.add(ambient)
 
-const sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96)
-sunLight.position.set(-3, 2, 3)
+const sunLight = new THREE.DirectionalLight(0xe8c37b, 1)
+sunLight.position.set(3, 2, 3)
 scene.add(sunLight)
 
 /////////////////////////////////////////////////////////////////////////
-///// LOADING GLB/GLTF MODEL FROM BLENDER
-// loader.load('models/gltf/starter-scene.glb', function (gltf) {
-
-//     scene.add(gltf.scene)
-// })
-let baseModel, baseTopFrame, baseBottomFrame, baseLeftFrame, baseRightFrame, centerTransom;
+///// LOADING GLB/GLTF MODEL
+let baseModel, baseTopFrame, baseBottomFrame, baseLeftFrame, baseRightFrame;
 const interactables = [];
 loader.load('models/shapedFrame.glb', function (gltf) {
     baseModel = gltf.scene;
@@ -108,10 +104,9 @@ function addCenterObject(LeftBound, RightBound) {
     const centerObject = new THREE.Mesh(geometry, material);
     centerObject.name = "center";
     centerObject.userData.transomAdded = false;
-    // centerObject.userData.centerObjectType = objectType;
     centerObject.userData.leftBound = LeftBound;
     centerObject.userData.rightBound = RightBound;
-    scene.add(centerObject);
+    baseModel.add(centerObject);
     var pos = [];
     pos.push(LeftBound);
     pos.push(RightBound);
@@ -120,56 +115,20 @@ function addCenterObject(LeftBound, RightBound) {
     interactables.push(centerObject);
 }
 
-//introAnimation() // call intro animation on start
-
 /////////////////////////////////////////////////////////////////////////
 //// DEFINE ORBIT CONTROLS LIMITS
 function setOrbitControlsLimits() {
     controls.enableDamping = true
     controls.dampingFactor = 0.04
-    controls.minDistance = 35
-    controls.maxDistance = 60
+    controls.minDistance = 1
+    controls.maxDistance = 10
     controls.enableRotate = true
     controls.enableZoom = true
-    controls.maxPolarAngle = Math.PI / 2.5
+    controls.maxPolarAngle = Math.PI / 2
 }
-
-/////////////////////////////////////////////////////////////////////////
-//////GUI 
-
-// var params = {
-//     color: sunLight.color.getHex(),
-//     color2: ambient.color.getHex(),
-//     color3: scene.background.getHex(),
-// }
-
-// // create a function to be called by GUI
-// const params_update = function () {
-//     var colorObj = new THREE.Color(params.color)
-//     var colorObj2 = new THREE.Color(params.color2)
-//     var colorObj3 = new THREE.Color(params.color3)
-//     sunLight.color.set(colorObj)
-//     ambient.color.set(colorObj2)
-//     scene.background.set(colorObj3)
-// }
-
-//////////////////////////////////////////////////
-//// GUI CONFIG
-//gui.add(baseModel.scale,'frameObjScale').name('Frame Resize').onChange(params_update)
-// gui.add(sunLight, 'intensity').min(0).max(10).step(0.0001).name('Dir intensity')
-// gui.add(sunLight.position, 'x').min(-100).max(100).step(0.00001).name('Dir X pos')
-// gui.add(sunLight.position, 'y').min(0).max(100).step(0.00001).name('Dir Y pos')
-// gui.add(sunLight.position, 'z').min(-100).max(100).step(0.00001).name('Dir Z pos')
-// gui.addColor(params, 'color').name('Dir color').onChange(params_update)
-// gui.addColor(params, 'color2').name('Amb color').onChange(params_update)
-// gui.add(ambient, 'intensity').min(0).max(10).step(0.001).name('Amb intensity')
-// gui.addColor(params, 'color3').name('BG color').onChange(params_update)
-
-/////////////////////////////////////////////////
 
 ////////////////////////////////////////////
 //// Events
-const transomModels = [];
 const v = new THREE.Vector2()
 renderer.domElement.addEventListener('click', onClick, false);
 function onClick(event) {
@@ -181,128 +140,27 @@ function onClick(event) {
 
     const intersects = raycaster.intersectObjects(interactables, true)
     if (intersects.length > 0) {
-        loader.load('models/transomsModel.glb', function (gltf) {
-            console.log(intersects[0])
-            var transomModel = gltf.scene;
-            transomModel.scale.y = 100;
-            scene.add(transomModel);
-            intersects[0].object.userData.transomAdded = true;
-            transomModel.position.copy(intersects[0].object.position);
-            addCenterObject(transomModel.position,intersects[0].object.userData.leftBound);
-            addCenterObject(transomModel.position,intersects[0].object.userData.rightBound);
-        });
-
-        // if (intersects[0].object.userData.centerObjectType == 0 &&  !intersects[0].object.userData.transomAdded) {
-        //     loader.load('models/transomsModel.glb', function (gltf) {
-        //         var transomModel = gltf.scene;
-        //         transomModel.scale.y = 100;
-        //         scene.add(transomModel);
-
-        //         // const geometry = new THREE.BoxGeometry(.1, .1, .1);
-        //         // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        //         // const centerObject = new THREE.Mesh(geometry, material);
-        //         // centerObject.name = "center";
-        //         // scene.add(centerObject);
-        //         // var transomModel = gltf.scene;
-        //         // transomModel.scale.y = 100;
-        //         // transomModels.push(transomModel);
-        //         // scene.add(transomModel);
-        //         // centerTransom = transomModel;
-        //         // intersects[0].object.userData.transomAdded = true;
-
-        //         // transomModel.position.copy(intersects[0].object.position);
-        //         // console.log("transom model", transomModel, intersects[0]);
-        //         // // intersects[0].object.visible = false;
-        //         // var pos = [];
-        //         // pos.push(transomModel.position);
-        //         // pos.push(baseLeftFrame.position);
-        //         // new THREE.Box3().setFromPoints(pos).getCenter(centerObject.position).multiplyScalar(1);
-        //         // centerObject.userData.centerObjectType = 1
-        //         // centerObject.userData.transomAdded = false
-        //         // centerObject.userData.box3 = pos
-        //         // interactables.push(centerObject);
-        //         // var pos1 = [];
-        //         // pos1.push(transomModel.position);
-        //         // pos1.push(baseRightFrame.position);
-        //         // var centerObject2 = centerObject.clone();
-        //         // new THREE.Box3().setFromPoints(pos1).getCenter(centerObject2.position).multiplyScalar(1);
-        //         // interactables.push(centerObject2);
-        //         // centerObject2.userData.centerObjectType = 2
-        //         // centerObject2.userData.transomAdded = false
-        //         // centerObject2.userData.box3 = pos1
-        //         // scene.add(centerObject2);
-        //     })
-
-        // }
-        // if (intersects[0].object.userData.centerObjectType == 1 && !intersects[0].object.userData.transomAdded) {
-        //     loader.load('models/transomsModel.glb', function (gltf) {
-        //         const geometry = new THREE.BoxGeometry(.1, .1, .1);
-        //         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        //         const centerObject = new THREE.Mesh(geometry, material);
-        //         centerObject.name = "center";
-        //         scene.add(centerObject);
-        //         var transomModel = gltf.scene;
-        //         transomModel.scale.y = 100;
-        //         transomModels.push(transomModel);
-        //         scene.add(transomModel);
-        //         intersects[0].object.userData.transomAdded = true;
-
-        //         transomModel.position.copy(intersects[0].object.position);
-        //         console.log("transom model", transomModel, intersects[0]);
-        //         // intersects[0].object.visible = false;
-        //         var pos = [];
-        //         pos.push(transomModel.position);
-        //         pos.push(baseLeftFrame.position);
-        //         new THREE.Box3().setFromPoints(pos).getCenter(centerObject.position).multiplyScalar(1);
-        //         centerObject.userData.centerObjectType = 1
-        //         centerObject.userData.transomAdded = false
-        //         interactables.push(centerObject);
-        //         var pos1 = [];
-        //         pos1.push(transomModel.position);
-        //         pos1.push(centerTransom.position);
-        //         var centerObject2 = centerObject.clone();
-        //         new THREE.Box3().setFromPoints(pos1).getCenter(centerObject2.position).multiplyScalar(1);
-        //         interactables.push(centerObject2);
-        //         centerObject2.userData.centerObjectType = 2
-        //         centerObject2.userData.transomAdded = false
-        //         scene.add(centerObject2);
-        //     })
-        // }
-        // if (intersects[0].object.userData.centerObjectType == 2 && !intersects[0].object.userData.transomAdded) {
-        //     loader.load('models/transomsModel.glb', function (gltf) {
-        //         const geometry = new THREE.BoxGeometry(.1, .1, .1);
-        //         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        //         const centerObject = new THREE.Mesh(geometry, material);
-        //         centerObject.name = "center";
-        //         scene.add(centerObject);
-        //         var transomModel = gltf.scene;
-        //         transomModel.scale.y = 100;
-        //         transomModels.push(transomModel);
-        //         scene.add(transomModel);
-        //         intersects[0].object.userData.transomAdded = true;
-
-        //         transomModel.position.copy(intersects[0].object.position);
-        //         console.log("transom model", transomModel, intersects[0]);
-        //         // intersects[0].object.visible = false;
-        //         var pos = [];
-        //         pos.push(transomModel.position);
-        //         pos.push(centerTransom.position);
-        //         new THREE.Box3().setFromPoints(pos).getCenter(centerObject.position).multiplyScalar(1);
-        //         centerObject.userData.centerObjectType = 1
-        //         centerObject.userData.transomAdded = false
-        //         interactables.push(centerObject);
-        //         var pos1 = [];
-        //         pos1.push(transomModel.position);
-        //         pos1.push(baseRightFrame.position);
-        //         var centerObject2 = centerObject.clone();
-        //         new THREE.Box3().setFromPoints(pos1).getCenter(centerObject2.position).multiplyScalar(1);
-        //         interactables.push(centerObject2);
-        //         centerObject2.userData.centerObjectType = 2
-        //         centerObject2.userData.transomAdded = false
-        //         scene.add(centerObject2);
-        //     })
-        // }
-
+        if (!intersects[0].object.userData.transomAdded) {
+            loader.load('models/transomsModel.glb', function (gltf) {
+                console.log(intersects[0])
+                var transomModel = gltf.scene;
+                transomModel.scale.y = 100;
+                baseModel.add(transomModel);
+                intersects[0].object.userData.transomModel = transomModel;
+                intersects[0].object.userData.transomAdded = true;
+                transomModel.position.copy(intersects[0].object.position);
+                addCenterObject(transomModel.position, intersects[0].object.userData.leftBound);
+                addCenterObject(transomModel.position, intersects[0].object.userData.rightBound);
+            });
+        }
+        else {
+            if (intersects[0].object.userData.transomModel.visible) {
+                intersects[0].object.userData.transomModel.visible = false;
+            }
+            else {
+                intersects[0].object.userData.transomModel.visible = true;
+            }
+        }
     }
 }
 
@@ -325,27 +183,3 @@ function rendeLoop() {
 }
 
 rendeLoop() //start rendering
-
-
-////get center point of mesh
-/**
- * Compute the center of a THREE.Group by creating a bounding box
- * containing every children's bounding box.
- * @param {THREE.Group} group - the input group
- * @param {THREE.Vector3=} optionalTarget - an optional output point
- * @return {THREE.Vector3} the center of the group
- */
-var computeGroupCenter = (function () {
-    return function (group, optionalTarget) {
-        if (!optionalTarget) optionalTarget = new THREE.Vector3();
-        new THREE.Box3().setFromObject(group).getCenter(optionalTarget).multiplyScalar(-1);
-        return optionalTarget;
-    }
-})();
-
-
-var computeCenterOfTwoMesh = (function name() {
-    return function (group1, group2) {
-
-    }
-})();
